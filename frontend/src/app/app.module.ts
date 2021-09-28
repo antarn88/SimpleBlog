9ccffda51f16290373fd2b6057467dc924944475
Arnold <antaloczi.arnold@gmail.com>
2021-09-28 17:31:00 +0200

    Frontend: Added main components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './page/login/login.component';
+import { HomeComponent } from './page/home/home.component';
+import { ProfileComponent } from './page/profile/profile.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { FooterComponent } from './widget/footer/footer.component';
+import { NavComponent } from './widget/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProfileComponent,
+    AdminComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
